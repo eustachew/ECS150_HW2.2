8bc088b2fc12162b2c61c71a3ac21b30e0889cd0
Oscar Pineda <74656047+Ace9414@users.noreply.github.com>
2025-05-12 14:54:16 -0700

    sem.c updated

--- libuthread/sem.c
+++ libuthread/sem.c
@@ -7,25 +7,80 @@
 
 struct semaphore {
 	/* TODO Phase 3 */
+	size_t count;
+	queue_t waitQueue;
+
 };
 
 sem_t sem_create(size_t count) //Do
 {
 	/* TODO Phase 3 */
+	sem_t sem = (sem_t)malloc(sizeof(struct semaphore));
+	if(!sem)
+		return NULL;
+	sem->count = count;
+	sem->waitQueue = queue_create();
+	
+
+	return sem;
+
 }
 
 int sem_destroy(sem_t sem) //Do
 {
 	/* TODO Phase 3 */
+	if (!sem || !queue_is_empty(sem->waitQueue))
+		return -1; // Don't destroy if there are waiting threads
+
+	
+	queue_destroy(sem->waitQueue);
+	free(sem);
+
+	return 0;
+
 }
 
 int sem_down(sem_t sem)
 {
 	/* TODO Phase 3 */
+	if (!sem)
+		return -1;
+	
+	preempt_disable();
+
+	
+	if (sem->count > 0) {
+		sem->count--;
+		preempt_enable();
+	} 
+		else {
+		queue_enqueue(sem->waitQueue, uthread_current);
+		uthread_block(); //swithces context
+
+		sem->count--;
+	}
+
+	preempt_enable();
+	return 0;
+	
+
+
+
+
+
+
 }
 
 int sem_up(sem_t sem)
 {
 	/* TODO Phase 3 */
+
+	if (!sem)
+		return -1;
+	
+	preempt_disable();
+
+	
+
 }
 
